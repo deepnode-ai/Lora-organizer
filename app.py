a8b202a85eebef77164207afcff35a4c57da3bdc
Maximus <93272190+deepnode-ai@users.noreply.github.com>
2024-06-13 22:44:36 +0400

    Update app.py dynamic path

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-import streamlit as st
 import os
 from PIL import Image
+import streamlit as st
 
-# Function to recursively load images from "loras" and its subdirectories
+# Determine the directory of the currently executing script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Path to the 'loras' directory
+loras_path = os.path.join(script_dir, 'loras')
+
+# Function to recursively load images from 'loras' and its subdirectories
 def load_images(directory):
     images = []
+    print(f"Attempting to load images from directory: {directory}")
+    if not os.path.exists(directory):
+        print(f"Directory not found: {directory}")
+        return images
     for root, dirs, files in os.walk(directory):
         for filename in files:
             if filename.endswith(".jpeg"):  # Target specific image file extensions
@@ -22,14 +32,13 @@ def display_images(images):
     for idx, (filename, image_path, lora_path) in enumerate(images):
         with cols[idx % cols_per_row]:
             image = Image.open(image_path)
+            image.thumbnail((200, 200))
             st.image(image, caption=filename, use_column_width=True)
             if st.button(f"Add lora", key=filename):
-                # Strip '.safetensors' from the lora_path and format the display name
                 display_name = os.path.splitext(lora_path)[0]
                 lora_display_name = f"<{display_name}:1.0>"
                 st.session_state['lora_display_name'] = lora_display_name
                 st.session_state['show_details'] = True
-
 
 # Navigation setup
 st.sidebar.title('Navigation')
@@ -40,7 +49,7 @@ if choice == 'Home':
     st.write('Welcome to the Home Page!')
 elif choice == 'Lora':
     st.header('Lora Files')
-    images = load_images('loras')
+    images = load_images(loras_path)
     display_images(images)
     st.write('Manage your Lora here.')
 elif choice == 'Checkpoint':
@@ -55,4 +64,3 @@ if 'show_details' in st.session_state and st.session_state.show_details:
         st.write(st.session_state.lora_display_name)
         if st.button("Close Details"):
             st.session_state.show_details = False
-
